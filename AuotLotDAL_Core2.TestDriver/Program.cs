734886c85c02f57b97aaf48e9a534a8c0f61ecbb
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-10-08 18:55:30 +0400

    .NET Core - EF

---
+++ AuotLotDAL_Core2.TestDriver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuotLotDAL_Core2.TestDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- AutoLotDAL_Core2/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AutoLotDAL_Core2
-{
-    public class Class1
-    {
-    }
-}
